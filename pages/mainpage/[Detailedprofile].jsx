efd1e940cc340b8efdf8630c72b4a21ab383653a
Satyam Mandal <satyam.mandal.77@gmail.com>
2021-10-01 14:47:01 +0530

    "Loader Add"

--- pages/mainpage/[Detailedprofile].jsx
+++ pages/mainpage/[Detailedprofile].jsx
@@ -6,73 +6,89 @@ import { useRouter } from "next/router"
 function Detailedprofile({ post }) {
     const router = useRouter()
     if (router.isFallback) {
-        return <h1>Loading ...</h1>
+        return (
+            <div className={Customstyle.spinner}>
+                <span className={Customstyle.spinnerinner1}></span>
+                <span className={Customstyle.spinnerinner2}></span>
+                <span className={Customstyle.spinnerinner3}></span>
+            </div>
+        )
     }
     return (
         <div className={Customstyle.frame}>
-            <div className={Customstyle.center}>
-                <div className={Customstyle.card}>
-                    <div className={Customstyle.left}>
-                        <div className={Customstyle.avatar}>
-                            <Image src={Profileimage} />
-                        </div>
-                        <div className={Customstyle.info}>
-                            <div><span className={Customstyle.big}>Name : </span><span className={Customstyle.small}>{post[0].name}</span></div>
-                            <div><span className={Customstyle.big}>User Name : </span><span className={Customstyle.small}>{post[0].username}</span></div>
-                            <div><span className={Customstyle.big}>Email : </span><span className={Customstyle.small}>{post[0].email}</span></div>
-                            <div><span className={Customstyle.big}>Phone  : </span><span className={Customstyle.small}>{post[0].phone}</span></div>
-                            <div><span className={Customstyle.big}>Website : </span><span className={Customstyle.small}>{post[0].website}</span></div>
-                        </div>
-                    </div>
-                    <div className={Customstyle.right}>
-                        <div className={Customstyle.stats}>
-                            <small>Company Information</small>
-                            <div>
-                                <span>Company Name</span>
-                                <span>{post[0].company.name}</span>
+            {post.length > 0 ?
+                <div className={Customstyle.center}>
+                    <div className={Customstyle.card}>
+                        <div className={Customstyle.left}>
+                            <div className={Customstyle.avatar}>
+                                <Image src={Profileimage} />
                             </div>
-                            <div>
-                                <span>Catch Phrase</span>
-                                <span>{post[0].company.catchPhrase}</span>
-                            </div>
-                            <div>
-                                <span>B.S</span>
-                                <span>{post[0].company.bs}</span>
+                            <div className={Customstyle.info}>
+                                <div><span className={Customstyle.big}>Name : </span><span className={Customstyle.small}>{post[0].name}</span></div>
+                                <div><span className={Customstyle.big}>User Name : </span><span className={Customstyle.small}>{post[0].username}</span></div>
+                                <div><span className={Customstyle.big}>Email : </span><span className={Customstyle.small}>{post[0].email}</span></div>
+                                <div><span className={Customstyle.big}>Phone  : </span><span className={Customstyle.small}>{post[0].phone}</span></div>
+                                <div><span className={Customstyle.big}>Website : </span><span className={Customstyle.small}>{post[0].website}</span></div>
                             </div>
                         </div>
-                        <div className={Customstyle.stats}>
-                            <small>Address Information</small>
-                            <div>
-                                <span>Address: </span>
-                                <span >{post[0].address.city + " " + post[0].address.street + " " + post[0].address.suite}</span>
+                        <div className={Customstyle.right}>
+                            <div className={Customstyle.stats}>
+                                <small>Company Information</small>
+                                <div>
+                                    <span>Company Name</span>
+                                    <span>{post[0].company.name}</span>
+                                </div>
+                                <div>
+                                    <span>Catch Phrase</span>
+                                    <span>{post[0].company.catchPhrase}</span>
+                                </div>
+                                <div>
+                                    <span>B.S</span>
+                                    <span>{post[0].company.bs}</span>
+                                </div>
                             </div>
-                            <div>
-                                <span>ZipCode: </span>
-                                <span >{post[0].address.zipcode}</span>
-                            </div>
-                            <div>
-                                <span>Geo: </span>
-                                <span >{post[0].address.geo.lat + " , " + post[0].address.geo.lng}</span>
+                            <div className={Customstyle.stats}>
+                                <small>Address Information</small>
+                                <div>
+                                    <span>Address: </span>
+                                    <span >{post[0].address.city + " " + post[0].address.street + " " + post[0].address.suite}</span>
+                                </div>
+                                <div>
+                                    <span>ZipCode: </span>
+                                    <span >{post[0].address.zipcode}</span>
+                                </div>
+                                <div>
+                                    <span>Geo: </span>
+                                    <span >{post[0].address.geo.lat + " , " + post[0].address.geo.lng}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                : <h1>No Data Found ....</h1>
+            }
         </div>
     )
 }
 export default Detailedprofile;
 
 export async function getStaticProps(context) {
-    const { params } = context;
-    const { Detailedprofile } = params;
-    console.log("Generate id => ", Detailedprofile)
-    const res = await fetch(
-        `${process.env.baseurl.detail}?id=${Detailedprofile}`
-    )
-    const data = await res.json();
-    return {
-        props: { post: data }
+    try {
+        const { params } = context;
+        const { Detailedprofile } = params;
+        console.log("Generate id => ", Detailedprofile)
+        const res = await fetch(
+            `${process.env.baseurl.detail}?id=${Detailedprofile}`
+        )
+        const data = await res.json();
+        return {
+            props: { post: data }
+        }
+    } catch (e) {
+        console.log("Error=> ", e);
+        return {
+            props: { post: [] }
+        }
     }
 }
 
